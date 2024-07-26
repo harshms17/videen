14b3ea1b900acd076f5bd9f64741fa6c8b0555bb
Harshms17 <146405467+Harshms17@users.noreply.github.com>
2024-07-26 21:00:40 +0530

    dropbox fix

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ const num3=document.querySelector('#nmbr3')
 
 const arr=[{elem:num1,val:18},{elem:num2,val:15},{elem:num3,val:523}]
 arr.forEach(component => {
-    const myloop =setInterval(()=>{
+    setInterval(()=>{
             let increment = Math.floor(component.elem.innerHTML)+Math.floor(Math.random()*component.val/5)
             if(increment>component.val){
                 increment = component.val
-                clearInterval(myloop)
+                clearInterval(this)
             }
             component.elem.innerHTML = increment
         },150)
+    
 })
